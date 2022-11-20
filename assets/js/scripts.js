e8beb36e37c8b697a4b97d4c3d2b1626da8f82a6
nurislam <nurislamnarbaev7@gmail.com>
2022-11-20 16:15:16 +0600

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,53 @@
+setTimeout(function(){
+    $('.odometer.01').html(23578);
+  }, 1000);
+  setTimeout(function(){
+    $('.odometer.02').html(12702);
+  }, 1000);
+  
+  
+ 
+  
+  jQuery(document).ready(function() {
+    jQuery('.image-gallery').magnificPopup({
+      delegate: '.item a', 
+      type:'image'
+    });
+  
+ 
+  
+    new WOW().init();
+  
+  });
+  
+  
+  
+
+  
+  $(function(){
+  
+    $('nav.pushy a[href*=#]').click(function() {
+  
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+  
+            var $target = $(this.hash);
+  
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+  
+            if ($target.length) {
+  
+                var targetOffset = $target.offset().top -0;
+  
+                $('html,body').animate({scrollTop: targetOffset}, 800);
+  
+                return false;
+  
+            }
+  
+        }
+  
+    });
+  
+  });
+  
